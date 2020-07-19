33ae4d0765df51fa4125ab4b315952c7a5824057
Peter Ray Shen <peterrayshen@gmail.com>
2020-07-19 03:32:12 -0400

    update ga

--- WebApp/public/js/GeneticAlgo.js
+++ WebApp/public/js/GeneticAlgo.js
@@ -7,6 +7,14 @@ class Beat {
     // sequence property (array of 1s and 0s)
     // score property
 
+    constructor(newBeat) {
+        this.sequence = [];
+        for (var i = 0; i < newBeat.data.length; i++) {
+            this.sequence.push(parseInt(newBeat.data[i]))
+        }
+        this.rating = newBeat.rating;
+    }
+
     mutate(mutationRate) {
         for (var i = 0; i < this.sequence.length; i++) {
             if (Math.random() < mutationRate) {
@@ -33,19 +41,20 @@ const generateRandomBeat = () => {
 }
 
 // population is an array of Beat
-const newGeneration = () => {
-    console.log("helloooo");
+const newGeneration = (beats) => {
     var population = [];
 
-    for (var i = 0; i < poplutaionSize; i++) {
-        population.push(generateRandomBeat());
+    for (var i = 0; i < beats.length; i++) {
+        population.push(new Beat(beats[i]));
     }
 
-    console.log(population);
     var matingPool = selection(population);
     newPoplution = generate(population, matingPool);
-    console.log(newPoplution);
-    return newPoplution;
+    formatted = [];
+    newPoplution.forEach(element => {
+        formatted.push({data: element.sequence.toString(), rating: element.rating});
+    })
+    return formatted;
 }
 
 // population is a list of beats
@@ -53,7 +62,7 @@ const selection = (population) => {
     var matingPool = [];
     population.forEach(element => {
         // for example, if beat 1 scores a 5/5, beat 1 is replicated 5 times in the mating pool
-        for (var i = 0; i < element.score; i++) {
+        for (var i = 0; i < element.rating; i++) {
             matingPool.push(element)
         }
     });
@@ -75,18 +84,17 @@ const generate = (population, matingPool) => {
 }
 
 const crossover = (parentA, parentB) => {
-    let child = new Beat();
-    child.sequence = [];
-    child.score = 1;
+    newSequence = [];
     for (var i = 0; i < parentA.sequence.length; i++) {
         // first half of sequence from parentA, second half from parentB
         if (i < parentA.sequence.length / 2) {
-            child.sequence.push(parentA.sequence[i]);
+            newSequence.push(parentA.sequence[i]);
         }
         else {
-            child.sequence.push(parentB.sequence[i]);
+            newSequence.push(parentB.sequence[i]);
         }
     }
+    let child = new Beat({data: newSequence, rating: 1});
     return child;
 }
 
@@ -94,4 +102,12 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-newGeneration();
+newPopulation = newGeneration([{
+    data: '101010',
+    rating: 4
+    }, {
+    data: '111010',
+    rating: 1
+    }]);
+
+console.log(newPopulation); // eg. {data: "1,0,1,0,1,0", rating: 1}, {data: "1,0,1,0,1,0", rating: 1}
