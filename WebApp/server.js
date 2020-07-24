905b707bef3d62f923051998b5795fed7df4dc71
dbhardwa <33157626+dbhardwa@users.noreply.github.com>
2020-07-24 11:57:10 -0400

    Add files via upload

--- WebApp/server.js
+++ WebApp/server.js
@@ -5,23 +5,18 @@ const app = express();
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendfile(path.join(__dirname, 'stepSequencer.html'));
+  res.sendfile(path.join(__dirname, 'index-alt1.html'));
 });
 
-app.get('/stepsequencer', (req, res) => {
-  res.sendfile(path.join(__dirname, 'stepSequencer.html'));
+app.get('/alt2', (req, res) => {
+  res.sendfile(path.join(__dirname, 'index-alt2.html'));
 });
 
 app.get('/ga', (req, res) => {
   res.sendfile(path.join(__dirname, 'ga.html'));
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-
-app.listen(port, () => {
+app.listen(8000, () => {
   console.log('StepSequencer hosted on http://localhost:8000/')
   console.log('GA hosted on http://localhost:8000/ga')
 });
