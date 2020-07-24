ea72d5cf3ea113376de17dcaee16db8d4d8807e3
dbhardwa <33157626+dbhardwa@users.noreply.github.com>
2020-07-24 12:56:49 -0400

    Add files via upload

--- WebApp/server.js
+++ WebApp/server.js
@@ -8,20 +8,23 @@ app.get('/', (req, res) => {
   res.sendfile(path.join(__dirname, 'index-alt1.html'));
 });
 
+app.get('/alt1', (req, res) => {
+  res.sendfile(path.join(__dirname, 'index-alt1.html'));
+});
+
 app.get('/alt2', (req, res) => {
   res.sendfile(path.join(__dirname, 'index-alt2.html'));
+});
+
+app.get('/alt3', (req, res) => {
+  res.sendfile(path.join(__dirname, 'index-alt3.html'));
 });
 
 app.get('/ga', (req, res) => {
   res.sendfile(path.join(__dirname, 'ga.html'));
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-
-app.listen(port, () => {
+app.listen(8000, () => {
   console.log('StepSequencer hosted on http://localhost:8000/')
   console.log('GA hosted on http://localhost:8000/ga')
 });
